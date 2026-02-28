b7a079a2813d9295e1e88bb21934af45129bb578
FiliDalzi <fili.dalzi@gmail.com>
2026-02-28 23:15:13 +0100

    Update SingleIncludeImpls.cpp

--- Animations/src/SingleIncludeImpls.cpp
+++ Animations/src/SingleIncludeImpls.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
 #include "core.h"
 #define GABE_CPP_PRINT_IMPL
-#include <cppUtils/cppPrint.hpp>
+#include <cppUtilisLocal/cppPrint.hpp>
 #undef GABE_CPP_PRINT_IMPL
 
 #define GABE_CPP_UTILS_IMPL
-#include <cppUtils/cppUtils.hpp>
+#include <cppUtilisLocal/cppUtils.hpp>
 #undef GABE_CPP_UTILS_IMPL
 
 #define GABE_CPP_TESTS_IMPL
-#include <cppUtils/cppTests.hpp>
+#include <cppUtilisLocal/cppTests.hpp>
 #undef GABE_CPP_TESTS_IMPL
 
 #define GABE_CPP_STRING_IMPL
-#include <cppUtils/cppStrings.hpp>
+#include <cppUtilisLocal/cppStrings.hpp>
 #undef GABE_CPP_STRING_IMPL
 
 #define STB_IMAGE_IMPLEMENTATION
